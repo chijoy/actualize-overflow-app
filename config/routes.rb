a75aba51ee1f1f1cba6ce9a6a8c129fd8fbcb6ce
Joy <joymccaffreychicago@gmail.com>
2017-05-27 15:50:15 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/' => 'questions#index'
+  get '/questions' => 'questions#index'
+  get '/questions/new' => 'questions#new'
+  post '/questions' => 'questions#create'
+
+  get '/questions/:id' => 'questions#show'
+
+  get '/questions/:id/edit' => 'questions#edit'
+  patch '/questions/:id' => 'questions#update'
+
+  delete '/questions/:id' => 'questions#destroy'
 end
