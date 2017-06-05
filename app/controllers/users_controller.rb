f9b7c39856e5da8fe5e2e2fe9f708d0e9ab97207
Joy <joymccaffreychicago@gmail.com>
2017-06-05 14:19:05 -0500

    Add user error messages to sign up.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,22 +9,23 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(name: params[:name],
+    @user = User.new(name: params[:name],
                     email: params[:email],
                     password: params[:password],
                     password_confirmation: params[:password_confirmation]
                     )
 
-    if user.save
+    if @user.save
       session[:user_id] = user.id
       flash[:success] = "Yay! You created an account!"
       redirect_to '/'
     else
-      flash[:warning] = "Whoa there, something went wrong."
-      redirect_to '/signup'
+      @user.errors.full_messages
+      render 'new.html.erb'
     end
   end
 
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,8 +2,10 @@ class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
 
+  validates :solution, presence: true
+
   def posted_by
     user.name
   end
-  
+
 end
