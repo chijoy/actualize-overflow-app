5620da257386dfbdfd38fe4f701f9bb9d356e3ca
Joy <joymccaffreychicago@gmail.com>
2017-07-16 14:25:45 -0500

    Fix edit user password bug.

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 gem 'bootstrap-sass', '~> 3.3.6'
+gem 'sendgrid-ruby'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'rails_12factor', group: :production
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   # validates :email, uniqueness: true
-  validates :password, presence: true
+  validates :password, presence: true, on: :create
   # validates :password, length: {6..20}
 end
